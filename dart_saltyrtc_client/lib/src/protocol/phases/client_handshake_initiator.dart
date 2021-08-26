b4afb9713fda73e45615806dd2dd1928eafb361c
Andrea Corradi <andrea.corradi@xayn.com>
2021-08-26 15:56:23 +0200

    Common part of ClientHandshakePhase

---
+++ dart_saltyrtc_client/lib/src/protocol/phases/client_handshake_initiator.dart
@@ -0,0 +1,26 @@
+import 'dart:typed_data' show Uint8List;
+
+import 'package:dart_saltyrtc_client/src/messages/message.dart' show Message;
+import 'package:dart_saltyrtc_client/src/messages/nonce/nonce.dart' show Nonce;
+import 'package:dart_saltyrtc_client/src/protocol/phases/client_handshake.dart'
+    show ClientHandshakePhase;
+import 'package:dart_saltyrtc_client/src/protocol/phases/phase.dart'
+    show InitiatorPhase, InitiatorData, Common, Phase;
+
+class InitiatorClientHandshakePhase extends ClientHandshakePhase
+    with InitiatorPhase {
+  @override
+  final InitiatorData data;
+
+  InitiatorClientHandshakePhase(Common common, this.data) : super(common);
+
+  @override
+  void handleServerMessageOther(Message msg, Nonce nonce) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Phase handleClientMessage(Uint8List msgBytes, Nonce nonce) {
+    throw UnimplementedError();
+  }
+}
