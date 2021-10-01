af8241a0788d60d3bff74d6e2619a2310f75fa66
Philipp Korber <philipp@korber.dev>
2021-10-01 15:30:39 +0200

    Added events based on status codes.

--- dart_saltyrtc_client/lib/src/protocol/events.dart
+++ dart_saltyrtc_client/lib/src/protocol/events.dart
@@ -4,9 +4,13 @@ import 'package:dart_saltyrtc_client/src/messages/close_code.dart'
     show CloseCode;
 import 'package:dart_saltyrtc_client/src/protocol/error.dart'
     show SaltyRtcError;
+import 'package:equatable/equatable.dart' show Equatable;
 import 'package:meta/meta.dart' show immutable;
 
-abstract class Event {}
+abstract class Event extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 @immutable
 class ServerHandshakeDone extends Event {}
@@ -19,6 +23,9 @@ class ResponderAuthenticated extends Event {
   final Uint8List permanentKey;
 
   ResponderAuthenticated(this.permanentKey);
+
+  @override
+  List<Object?> get props => [permanentKey];
 }
 
 @immutable
@@ -55,3 +62,195 @@ class NoSharedTaskFound extends Event {
 //TODO use when we detect closing if the could not decrypt close code.
 @immutable
 class InitiatorCouldNotDecrypt extends Event {}
+
+/// The key we expect the server to use is not a key the server has.
+@immutable
+class IncompatibleServerKey extends Event {}
+
+enum TempFailureVariant {
+  /// We didn't responds with a pong to the servers ping.
+  timeout,
+
+  /// The WebRtc connection was closed without sending a close frame.
+  ///
+  /// This can e.g. happen in case of TCP realizing the connection
+  /// to the server is gone, it's inherently similar to `timeout`.
+  ///
+  /// While this is not part of the SaltyRtc protocol it can happen anyway.
+  abnormalClosure,
+
+  /// 253 responders are currently connected to the server on given path.
+  pathFull,
+
+  /// The initiator dropped us.
+  ///
+  /// This can happen in two cases:
+  ///
+  /// - Many responder try to connect to the path and we are dropped as part of
+  ///   the path cleaning.
+  ///
+  /// - The initiator is (was) paired with another responder, this should only be
+  ///   possible if multiple devices/responder have the same auth data (same auth
+  ///   token or same permanent keys). This case should normally not happen.
+  ///
+  /// While this error variant is likely temporary in case of the 2nd way it can
+  /// happen it would be potential permanent, but we can't detect it.
+  droppedByInitiator,
+
+  /// The service is restarting and will be available again soon.
+  ///
+  /// While this is not part of the SaltyRtc protocol it can happen anyway.
+  serviceRestart,
+
+  /// The services asks us to try again later.
+  ///
+  /// While this is not part of the SaltyRtc protocol it can happen anyway.
+  tryAgainLater,
+}
+
+/// A temporary failure happened.
+///
+/// Problems which are likely to go away if you try later again, it's *strongly*
+/// recommended to use a form of "back-off" strategy an not retry immediately.
+///
+@immutable
+class LikelyTemporaryFailure extends Event {
+  final TempFailureVariant variant;
+
+  LikelyTemporaryFailure(this.variant);
+
+  @override
+  List<Object?> get props => [variant];
+}
+
+enum UnexpectedStatusVariant {
+  /// The server or other client ran into a internal error.
+  ///
+  /// Either `1011` (WebRtc) or `3002` SaltyRtc.
+  internalError,
+
+  /// The server or other client found we breached to protocol.
+  ///
+  /// This can be a
+  ///
+  /// - `1002`: WebRtc protocol error
+  /// - `1003`: WebRtc unsupported data (should have been `3001`)
+  /// - `1007`: WebRtc invalid frame payload data (should have been `3001`)
+  /// - `3001`: SaltyRtc protocol error
+  ///
+  protocolError,
+
+  /// The TLS Handshake failed
+  tlsHandshake,
+
+  /// A received data frame was to large. (`1009`, WebRtc)
+  ///
+  /// It's very unlikely to happen with WebRtc, but one possible way
+  /// could be if the task data shipped with an `auth` message became
+  /// way to big.
+  messageToBig,
+
+  /// Any other unexpected status code.
+  ///
+  /// This includes following status codes, which all shouldn't happen
+  /// (at least in our use case):
+  ///
+  /// - `1005`: No Status Received
+  /// - `1008`: Policy violation
+  /// - `1010`: Missing Extension
+  /// - `1014`: Bad Gateway
+  other
+}
+
+/// A unexpected error occurred.
+///
+/// This is most likely a bug on at least one side, but in rare cases can also
+/// be caused by bad `pingInterval` settings and/or unusual network conditions.
+///
+/// Retrying is likely not going to help, through it might be worth to retry
+/// a single time with a larger time gap in case of internal error and
+/// other times after a huge time gap as the server might be fixed by then.
+@immutable
+class UnexpectedStatus extends Event {
+  final UnexpectedStatusVariant variant;
+  final int closeCode;
+
+  // Creates a instance, it doesn't check if `variant` matches `closeCode`.
+  //
+  // Preferably use `eventFromStatus` instead.
+  UnexpectedStatus.unchecked(
+    this.variant,
+    this.closeCode,
+  );
+
+  @override
+  List<Object?> get props => [variant, closeCode];
+}
+
+/// TODO: What exactly is this? It's currently unused? Or not?
+@immutable
+class HandoverOfSignalingChannel extends Event {}
+
+/// Event indicating that the web socket was closed.
+///
+/// This doesn't contain a specific reason as there could be multiple reasons,
+/// or no reason in case of a normal closing and appropriate events for this
+/// reasons will already have been emitted.
+///
+/// In a certain way this is a "end of stream" marker.
+@immutable
+class Closed extends Event {}
+
+/// Creates a event from an status code.
+///
+/// This can be used with WebRtc on-close status codes or codes send with a
+/// `close` message, because of that this will not create `Closed` events.
+/// This also means that a non (direct) error close code will not create an
+/// event (`normal` and `goingAway`).
+///
+Event? eventFromStatus(int closeCode) {
+  switch (closeCode) {
+    case 1000:
+    case 1001:
+      return null;
+    case 1002:
+    case 1003:
+    case 1007:
+    case 3001:
+      return UnexpectedStatus.unchecked(
+          UnexpectedStatusVariant.protocolError, closeCode);
+    case 1006:
+      return LikelyTemporaryFailure(TempFailureVariant.abnormalClosure);
+    case 1009:
+      return UnexpectedStatus.unchecked(
+          UnexpectedStatusVariant.messageToBig, closeCode);
+    case 1011:
+    case 3002:
+      return UnexpectedStatus.unchecked(
+          UnexpectedStatusVariant.internalError, closeCode);
+    case 1012:
+      return LikelyTemporaryFailure(TempFailureVariant.serviceRestart);
+    case 1013:
+      return LikelyTemporaryFailure(TempFailureVariant.tryAgainLater);
+    case 1015:
+      return UnexpectedStatus.unchecked(
+          UnexpectedStatusVariant.tlsHandshake, closeCode);
+    case 3000:
+      return LikelyTemporaryFailure(TempFailureVariant.pathFull);
+    case 3003:
+      return HandoverOfSignalingChannel();
+    case 3004:
+      return LikelyTemporaryFailure(TempFailureVariant.droppedByInitiator);
+    case 3005:
+      return InitiatorCouldNotDecrypt();
+    case 3006:
+      return NoSharedTaskFound();
+    case 3007:
+      return IncompatibleServerKey();
+    case 3008:
+      return LikelyTemporaryFailure(TempFailureVariant.timeout);
+    default:
+      return UnexpectedStatus.unchecked(
+          UnexpectedStatusVariant.other, closeCode);
+  }
+}
---
+++ dart_saltyrtc_client/test/protocol/events_test.dart
@@ -0,0 +1,96 @@
+import 'package:dart_saltyrtc_client/src/protocol/events.dart'
+    show
+        HandoverOfSignalingChannel,
+        IncompatibleServerKey,
+        InitiatorCouldNotDecrypt,
+        LikelyTemporaryFailure,
+        NoSharedTaskFound,
+        TempFailureVariant,
+        UnexpectedStatus,
+        UnexpectedStatusVariant,
+        eventFromStatus;
+import 'package:test/test.dart';
+
+void main() {
+  test('eventFromCloseCode', () {
+    expect(eventFromStatus(1000), isNull);
+    expect(eventFromStatus(1001), isNull);
+    expect(
+        eventFromStatus(1002),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.protocolError, 1002)));
+    expect(
+        eventFromStatus(1003),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.protocolError, 1003)));
+    expect(
+        eventFromStatus(1004),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 1004)));
+    expect(
+        eventFromStatus(1005),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 1005)));
+    expect(eventFromStatus(1006),
+        equals(LikelyTemporaryFailure(TempFailureVariant.abnormalClosure)));
+    expect(
+        eventFromStatus(1007),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.protocolError, 1007)));
+    expect(
+        eventFromStatus(1008),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 1008)));
+    expect(
+        eventFromStatus(1009),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.messageToBig, 1009)));
+    expect(
+        eventFromStatus(1010),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 1010)));
+    expect(
+        eventFromStatus(1011),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.internalError, 1011)));
+    expect(eventFromStatus(1012),
+        equals(LikelyTemporaryFailure(TempFailureVariant.serviceRestart)));
+    expect(eventFromStatus(1013),
+        equals(LikelyTemporaryFailure(TempFailureVariant.tryAgainLater)));
+    expect(
+        eventFromStatus(1014),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 1014)));
+    expect(
+        eventFromStatus(1015),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.tlsHandshake, 1015)));
+    expect(eventFromStatus(3000),
+        equals(LikelyTemporaryFailure(TempFailureVariant.pathFull)));
+    expect(
+        eventFromStatus(3001),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.protocolError, 3001)));
+    expect(
+        eventFromStatus(3002),
+        equals(UnexpectedStatus.unchecked(
+            UnexpectedStatusVariant.internalError, 3002)));
+    expect(eventFromStatus(3003), equals(HandoverOfSignalingChannel()));
+    expect(eventFromStatus(3004),
+        equals(LikelyTemporaryFailure(TempFailureVariant.droppedByInitiator)));
+    expect(eventFromStatus(3005), equals(InitiatorCouldNotDecrypt()));
+    expect(eventFromStatus(3006), equals(NoSharedTaskFound()));
+    expect(eventFromStatus(3007), equals(IncompatibleServerKey()));
+    expect(eventFromStatus(3008),
+        equals(LikelyTemporaryFailure(TempFailureVariant.timeout)));
+
+    expect(
+        eventFromStatus(3009),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 3009)));
+    expect(
+        eventFromStatus(1016),
+        equals(
+            UnexpectedStatus.unchecked(UnexpectedStatusVariant.other, 1016)));
+  });
+}
